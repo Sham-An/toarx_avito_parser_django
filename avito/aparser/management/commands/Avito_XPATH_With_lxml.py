5861d05fa282ea94207b3d8d3a0a9ff2ac76a92d
Sham-An <shamraev@inbox.ru>
2021-10-29 20:49:58 +0300

    Avito_XPATH_With_lxml

--- avito/aparser/management/commands/Avito_XPATH_With_lxml.py
+++ avito/aparser/management/commands/Avito_XPATH_With_lxml.py
@@ -6,12 +6,15 @@ doc = lxml.html.fromstring(html.content)
 
 new_releases = doc.xpath('//div[@elementtiming="bx.catalog.container"]')[0] #Родительский контейнер
 
+
 items_id = new_releases.xpath('.//div[@data-item-id]')
 meta_items_id = new_releases.xpath('.//meta[@itemprop="description"]/@content')
+
 print(f'meta_items_id {meta_items_id}')
 print(f'ВСЕГО {len(items_id)} В {items_id}')
 titles = new_releases.xpath('.//div[substring(@class,1,13) ="iva-item-desc"]//text()')
 prices = new_releases.xpath('.//div[@data-item-id]//meta[@itemprop="price"]/@content')
+all_in_one = new_releases.xpath('//*[//div[@data-item-id]//div[@data-marker="item-date"]//preceding::div[@data-marker="item-line"]]')
 print(len(titles))
 print(len(prices))
 #tags = [tag.text_content() for tag in new_releases.xpath('.//div[@class="tab_item_top_tags"]')]
