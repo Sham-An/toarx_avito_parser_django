d1220341acafcbc274191cfc7c1432fdec176a19
Sham-An <shamraev@inbox.ru>
2021-11-21 22:48:33 +0300

    parse_avito  xpath OK! Multiprocess.py + VPN Choise

---
+++ avito/aparser/management/commands/VPN_Choise.py
@@ -0,0 +1,30 @@
+# import required modules
+# Есть на GIT https://github.com/Dayzpd/Python-Windscribe
+import os
+from time import sleep
+import random
+
+# list of VPN server codes
+codeList = ["TR", "US-C", "US", "US-W", "CA", "CA-W",
+            "FR", "DE", "NL", "NO", "RO", "CH", "GB", "HK"]
+
+try:
+
+    # connect to VPN
+    os.system("windscribe connect") #L_9613150717 P_Pr*75
+    while True:
+        # assigning a random VPN server code
+        choiceCode = random.choice(codeList)
+
+        # changing IP after a particular time period
+        sleep(random.randrange(120, 300))
+
+        # connecting to a different VPN server
+        print("!!! Changing the IP Address........")
+        os.system("windscribe connect " + choiceCode)
+
+except:
+
+    # disconnect VPN
+    os.system("windscribe disconnect")
+    print("sorry, some error has occurred..!!")
